004e0a5f80ecc0635294245e957f4835ba7907cc
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-10 18:49:11 +0300

    Create AuthenticationResponse.java

---
+++ emg-service-account/src/main/java/com/batuhan/emg_service_account/dto/response/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.batuhan.emg_service_account.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String accessToken;
+    private String tokenType;
+}
