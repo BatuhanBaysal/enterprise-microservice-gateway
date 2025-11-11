169dafd8bca6f2b86bb511010f1a7ef461bd8627
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-11 17:47:01 +0300

    Create AuthenticationController.java

---
+++ emg-service-account/src/main/java/com/batuhan/emg_service_account/controller/AuthenticationController.java
@@ -0,0 +1,44 @@
+package com.batuhan.emg_service_account.controller;
+
+import com.batuhan.emg_service_account.dto.request.LoginRequest;
+import com.batuhan.emg_service_account.dto.response.AuthenticationResponse;
+import com.batuhan.emg_service_account.service.JwtService;
+import com.batuhan.emg_service_account.service.AccountServiceImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManager;
+    private final AccountServiceImpl accountService;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@Valid @RequestBody LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        final UserDetails userDetails = accountService.loadUserByUsername(request.getUsername());
+        final String jwt = jwtService.generateToken(userDetails);
+
+        return ResponseEntity.ok(AuthenticationResponse.builder()
+                .accessToken(jwt)
+                .tokenType("Bearer")
+                .build());
+    }
+}
